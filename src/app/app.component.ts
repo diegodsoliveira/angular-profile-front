d681454edfe88370224f9ada20bfb335d2281575
Diego Oliveira <diegodsoliveira@gmail.com>
2024-07-08 19:00:32 -0300

    feat: states service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { StatesService } from './services/states.service';
 import { Component, OnInit } from '@angular/core';
 import { CountriesService } from './services/countries.service';
 
@@ -10,11 +11,15 @@ export class AppComponent implements OnInit {
   title = 'angular-profile-front';
 
   constructor(
-    private readonly _countriesService: CountriesService
+    private readonly _countriesService: CountriesService,
+    private readonly _statesService: StatesService
   ) { }
   ngOnInit() {
     this._countriesService.getCountries().subscribe((countriesResponse) => {
       console.log('countriesResponse', countriesResponse);
     });
+    this._statesService.getStates('Brazil').subscribe((statesResponse) => {
+      console.log('statesResponse', statesResponse);
+    })
   }
 }
